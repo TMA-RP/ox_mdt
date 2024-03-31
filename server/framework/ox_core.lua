0fc84e3b4c5998d54385e5d8ff506e5ec9ca22f0
Cyril Beeckman <cyril.beeckman@gmail.com>
2024-03-31 15:04:47 +0200

    fix license issued

--- server/framework/ox_core.lua
+++ server/framework/ox_core.lua
@@ -144,7 +144,7 @@ end
 ---@return table<string, { label: string } | string>[]
 function ox.getLicenses(parameters)
     local licenses = MySQL.rawExecute.await(
-        'SELECT ox_licenses.label, DATE_FORMAT(`issued`, "%d/%m/%Y") as issued FROM character_licenses LEFT JOIN ox_licenses ON ox_licenses.name = character_licenses.name WHERE `charid` = ?',
+        'SELECT ox_licenses.label, DATE_FORMAT(FROM_UNIXTIME(JSON_EXTRACT(character_licenses.data, "$.issued") / 1000), "%d/%m/%Y") as issued FROM character_licenses LEFT JOIN ox_licenses ON ox_licenses.name = character_licenses.name WHERE `charid` = ?',
         parameters) or {}
 
     return licenses
